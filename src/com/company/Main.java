4718a3012be0018311a4c8039a6242d73ec49a18
gonzalovillagransia <gonzalo.villagran.sia@gmail.com>
2021-02-18 19:38:34 +0100

    Ejercicio final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
             System.out.println("hola mundo");
             System.out.println("Que pasa mi gente");
-            System.out.println("Lo que paso ,paso");
+            System.out.println("Lo que paso ,paso,parapa");
     }
 }
