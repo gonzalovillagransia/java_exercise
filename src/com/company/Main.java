9cfdf3f0bbaff1b7819fd88d1369de650a85ca8a
gonzalovillagransia <gonzalo.villagran.sia@gmail.com>
2021-02-18 19:08:34 +0100

    Ejercicio version 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
             System.out.println("hola mundo");
-            System.out.println("Que pasa mi gent");
+            System.out.println("Que pasa mi gente");
+            System.out.println("Lo que paso paso");
     }
 }
