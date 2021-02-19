ccc6a2b9cba5aa526fe1cdc55aa0839fa3926bd9
gonzalovillagransia <gonzalo.villagran.sia@gmail.com>
2021-02-19 10:10:39 +0100

    Ultima version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Main {
 
+
     public static void main(String[] args) {
             System.out.println("hola mundo");
             System.out.println("Que pasa mi gente");
