ea68496f8d9063d952d5237189f229f8d3b2d339
gonzalovillagransia <gonzalo.villagran.sia@gmail.com>
2021-02-19 11:22:43 +0100

    Suma de dos numeros

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
-            System.out.println("hola mundo");
-            System.out.println("Que pasa mi gente");
-            System.out.println("Lo que paso ,paso,parapa");
+
+        int num1=0,num2=0, resultado=0;
+        Scanner objeto= new Scanner(System.in);
+
+            System.out.println("Introduzca el primer numero");
+            num1=objeto.nextInt();
+            System.out.println("Introduzca el primer numero");
+            num2=objeto.nextInt();
+
+            resultado=num1+num2;
+
+            System.out.println("El resultado es:" + resultado);
+
     }
 }
